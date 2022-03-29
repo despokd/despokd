b942f00e9c72a3a177c4514fe3902fc8e3353f95
Kilian Domaratius <github@kdomaratius.de>
2022-03-29 09:57:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,13 +26,15 @@ const readme = `\
 # <p align="center"> Hi, I'm Kilian <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/wave.gif" width="29px"></p>
 <p align="center">CV & Skills on <a href="https://kdomaratius.de">kdomaratius.de</a></p>
 <p align="center">Personal interests, code snippets and more on <a href="https://tequilian.de">tequilian.de</a></p>
+<p align="center">Work related stuff on <a href="https://gitlab.com/kdomaratius">Gitlab @kdomaratius</a></p>
+
 
 ### About Me
-- 🏦 I have recently completed my training as an IT specialist and create e-commerce / cms websites in Germany.
+- 🏦 I completed my training as an IT specialist and create e-commerce / cms websites in Germany.
       <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30">
 
 - 🔭 I’m currently working on my donations website [xmas-magic](https://github.com/despokd/xmas-magic)  
-- 🌱 I’m currently learning  Docker + Shopware 6
+- 🌱 I’m currently learning  Docker + Symfony
 - ❓ Ask me about anything related to Shopware 6 Frontend
 - 😄 Pronouns: he / his  
 
@@ -54,17 +56,6 @@ const readme = `\
   new Date().getFullYear() +
   "."
 }
-
----
-
-## Recent Blog Posts  
-<!-- BLOG-POST-LIST:START -->
-- [Portfolio / kdomaratius.de](https://tequilian.de/projekte/portfolio-kdomaratius-de/)
-- [Contact reader](https://tequilian.de/projekte/web-contact-reader/)
-- [Fix: SVG not shown in Firefox (Elementor)](https://tequilian.de/snippets/fix-svg-not-shown-in-firefox-elementor/)
-- [Add dark mode based on system preferences in Vuetify](https://tequilian.de/snippets/add-dark-mode-based-on-system-preferences-in-vuetify/)
-- [PrestaShop: More characters for article names](https://tequilian.de/snippets/prestashop-more-characters-for-article-name/)
-<!-- BLOG-POST-LIST:END -->  
 
 `;
 console.log(readme);
