a4ffb32c937855a25c5974ed4a13dd724de20648
Kilian Domaratius <github@kdomaratius.de>
2023-08-18 18:40:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,17 +26,13 @@ const readme = `\
 # <p align="center"> Hi, I'm Kilian <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/wave.gif" width="29px"></p>
 <p align="center">CV & Skills on <a href="https://kdomaratius.de">kdomaratius.de</a></p>
 <p align="center">Personal interests, code snippets and more on <a href="https://tequilian.de">tequilian.de</a></p>
-<p align="center">Work related stuff on <a href="https://gitlab.com/kdomaratius">Gitlab @kdomaratius</a></p>
-
 
 ### About Me
 - 🏦 I completed my training as an IT specialist and create e-commerce / cms websites in Germany.
       <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30">
-
+      
 - 🔭 I’m currently working on my private website [tequilian.de](https://github.com/despokd/tequilian)  
-- 🌱 I’m currently learning Docker + Symfony
-- ❓ Ask me about anything related to Shopware 6 Frontend
-- 😄 Pronouns: he / his  
+- ❓ Ask me about anything related to Shopware 6 Frontend & WordPress
 
 ---
 
